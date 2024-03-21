57484d27321747755ab049df6bc2bbf82600b0a1
duyhungg <dongocduyhung7@gmail.com>
2024-03-21 13:56:16 +0700

    update data

--- backend/seeder/data.js
+++ backend/seeder/data.js
@@ -4,7 +4,7 @@ export default [
     price: "1.099",
     description:
       " iPhone 14 Pro Max 256GB iPhone cao cấp với màn hình Super Retina XDR, chip A16 Bionic mạnh mẽ, hệ thống camera ba ống kính tiên tiến, và dung lượng lưu trữ 256GB, đảm bảo trải nghiệm đỉnh cao cho người dùng.",
-    ratings: 5.0,
+    ratings: 0.0,
     images: [
       {
         public_id: "Product",
@@ -47,7 +47,7 @@ export default [
     seller: "Apple",
     stock: 100,
     Sold:50,
-    numOfReviews: 32,
+    numOfReviews: 0,
     reviews: [],
   },
   {
@@ -55,7 +55,7 @@ export default [
     price: "200",
     description:
       "AirPods 3 là phiên bản tiếp theo của dòng tai nghe không dây nổi tiếng từ Apple. Với thiết kế cải tiến, chất lượng âm thanh nâng cao và tính năng hiện đại, AirPods 3 mang đến trải nghiệm nghe nhạc và gọi điện tối ưu hóa. Đặc biệt, chúng có thiết kế thoải mái với nhiều đầu nút tai nghe cho phù hợp với đa số người dùng.",
-    ratings: 4.5,
+    ratings: 0.0,
     images: [
       {
         public_id: "Product",
@@ -74,7 +74,7 @@ export default [
     seller: "Apple",
     stock: 100,
     Sold:52,
-    numOfReviews: 2,
+    numOfReviews: 0,
     reviews: [],
   },
   {
@@ -82,7 +82,7 @@ export default [
     price: "300",
     description:
       "AirPods Pro 2 là tai nghe true wireless mới nhất của Apple, được ra mắt vào tháng 12 năm 2023. Tai nghe này có thiết kế tương tự như AirPods Pro thế hệ đầu tiên, nhưng được trang bị nhiều tính năng mới và cải tiến, bao gồm khử tiếng ồn, âm thanh không gian. chế độ xuyên âm",
-    ratings: 3.5,
+    ratings: 0.0,
     images: [
       {
         public_id: "Product",
@@ -101,7 +101,7 @@ export default [
     seller: "Apple",
     stock: 100,
     Sold:50,
-    numOfReviews: 12,
+    numOfReviews: 0,
     reviews: [],
   },
   {
@@ -109,7 +109,7 @@ export default [
     price: "2099",
     description:
       "iPad Pro M1 12.9 inch Wifi là một chiếc máy tính bảng mạnh mẽ và linh hoạt, được trang bị con chip M1 của Apple. Máy có màn hình Liquid Retina XDR tuyệt đẹp, camera trước và sau chất lượng cao, và thời lượng pin dài. Sử dụng chip M1 của Apple",
-    ratings: 4.8,
+    ratings: 0.0,
     images: [
       {
         public_id: "Product",
@@ -120,7 +120,7 @@ export default [
     seller: "Apple",
     stock: 100,
     Sold:30,
-    numOfReviews: 2671,
+    numOfReviews: 0,
     reviews: [],
   },
   {
@@ -128,7 +128,7 @@ export default [
     price: "1499",
     description:
       "iPad Pro M2 12.9 inch 5G là chiếc máy tính bảng mạnh mẽ và linh hoạt nhất của Apple, được trang bị con chip M2 mới nhất của hãng. Máy có màn hình Liquid Retina XDR tuyệt đẹp, camera trước và sau chất lượng cao, và thời lượng pin dài.",
-    ratings: 4.1,
+    ratings: 0.0,
     images: [
       {
         public_id: "Product",
@@ -139,7 +139,7 @@ export default [
     seller: "Apple",
     stock: 100,
     Sold:500,
-    numOfReviews: 6,
+    numOfReviews: 0,
     reviews: [],
   },
   {
@@ -147,7 +147,7 @@ export default [
     price: "899",
     description:
       "IPhone 13 có thiết kế tương tự như iPhone 12, nhưng có một số thay đổi nhỏ. Cạnh máy được làm bằng thép không gỉ, mặt sau được làm bằng kính và có một camera kép ở góc trên cùng bên trái. Mặt trước được trang bị màn hình Super Retina XDR OLED 6,1 inch với độ phân giải 2532 x 1170 pixel.",
-    ratings: 4.7,
+    ratings: 0.0,
     images: [
       {
         public_id: "Product",
@@ -157,7 +157,7 @@ export default [
     category: "Phones",
     seller: "Apple",
     stock: 100,
-    numOfReviews: 222,
+    numOfReviews: 0,
     reviews: [],
   },
   {
@@ -165,7 +165,7 @@ export default [
     price: "21.690000",
     description:
       "IPhone 14 Plus có thiết kế tương tự như iPhone 13 Plus, nhưng có một số thay đổi nhỏ. Cạnh máy được làm bằng thép không gỉ, mặt sau được làm bằng kính và có hai camera ở góc trên cùng bên trái. Mặt trước được trang bị màn hình Super Retina XDR OLED 6,7 inch với độ phân giải 2778 x 1284 pixe",
-    ratings: 4.9,
+    ratings: 0.0,
     images: [
       {
         public_id: "Product",
@@ -175,7 +175,7 @@ export default [
     category: "Phones",
     seller: "Apple",
     stock: 100,
-    numOfReviews: 56,
+    numOfReviews: 0,
     reviews: [],
   },
   {
@@ -183,8 +183,8 @@ export default [
     price: "999",
     description:
       "iPhone 14 Pro có thiết kế tương tự như iPhone 13 Pro, nhưng có một số thay đổi nhỏ. Cụ thể, iPhone 14 Pro có màn hình notch được thay thế bằng thiết kế viên thuốc và camera đục lỗ sử dụng chip A16 bionic",
-    ratings: 5,
-    images: [
+    ratings: 0.0,
+        images: [
       {
         public_id: "Product",
         url: "https://res.cloudinary.com/daf4umk5i/image/upload/v1693391882/Products/iphone-14-pro-gold-thumbtz-650x650_dnuflx.webp",
@@ -193,7 +193,7 @@ export default [
     category: "Phones",
     seller: "Apple",
     stock: 500,
-    numOfReviews: 1,
+    numOfReviews: 0,
     reviews: [],
   },
   {
@@ -201,7 +201,7 @@ export default [
     price: "799",
     description:
       "iPhone 14 có thiết kế tương tự như iPhone 13, nhưng có một số thay đổi nhỏ. Cụ thể, iPhone 14 vẫn có màn hình notch, nhưng được thu nhỏ kích thước sử dụng chip A16 Bionic của apple",
-    ratings: 5,
+    ratings: 0.0,    
     images: [
       {
         public_id: "Product",
@@ -211,7 +211,7 @@ export default [
     category: "Phones",
     seller: "Apple",
     stock: 500,
-    numOfReviews: 1,
+    numOfReviews: 0,
     reviews: [],
   },
   {
@@ -219,7 +219,7 @@ export default [
     price: "1299",
     description:
       "MacBook Air 15 inch M2 2023 là một chiếc máy tính xách tay mới nhất của Apple, được công bố vào ngày 14 tháng 9 năm 2023. Máy có thiết kế mỏng nhẹ, hiệu suất mạnh mẽ và thời lượng pin dài.Màn hình có độ sáng tối đa lên đến 500nits ",
-    ratings: 5,
+    ratings: 0.0,    
     images: [
       {
         public_id: "Product",
@@ -229,7 +229,7 @@ export default [
     category: "Laptops",
     seller: "Apple",
     stock: 500,
-    numOfReviews: 1,
+    numOfReviews: 0,
     reviews: [],
   },
   {
@@ -237,8 +237,7 @@ export default [
     price: "1899",
     description:
       "MacBook Pro M2 14 inch là một chiếc máy tính xách tay mới nhất của Apple. Máy có thiết kế mới, hiệu suất mạnh mẽ và thời lượng pin dài.MacBook Pro M2 14 inch được trang bị màn hình Liquid Retina XDR 14,2 inch với độ phân giải 3024 x 1964 pixel. Màn hình này có độ sáng tối đa lên đến 1600 nits, giúp hiển thị hình ảnh sống động và rõ nét. Sử dụng chip M2 của Apple cho hiệu xuất mạnh mẽ",
-    ratings: 5,
-    images: [
+    ratings: 0.0,    images: [
       {
         public_id: "Product",
         url: "https://res.cloudinary.com/daf4umk5i/image/upload/v1693391854/Products/mac-pro-14-m2-bac-650x650_sgdh4i.webp",
@@ -247,7 +246,7 @@ export default [
     category: "Laptops",
     seller: "Apple",
     stock: 500,
-    numOfReviews: 1,
+    numOfReviews: 0,
     reviews: [],
   },
   {
@@ -255,8 +254,7 @@ export default [
     price: "2099",
     description:
       "MacBook Pro M2 14 inch là một chiếc máy tính xách tay mới nhất của Apple. Máy có thiết kế mới, hiệu suất mạnh mẽ và thời lượng pin dài.MacBook Pro M2 14 inch được trang bị màn hình Liquid Retina XDR 14,2 inch với độ phân giải 3024 x 1964 pixel. Màn hình này có độ sáng tối đa lên đến 1600 nits, giúp hiển thị hình ảnh sống động và rõ nét. Sử dụng chip M2 của Apple cho hiệu xuất mạnh mẽ",
-    ratings: 5,
-    images: [
+    ratings: 0.0,   images: [
       {
         public_id: "Product",
         url: "https://res.cloudinary.com/daf4umk5i/image/upload/v1693391838/Products/mac-pro-16-m2-xam-650x650_eakgkm.webp",
@@ -265,7 +263,7 @@ export default [
     category: "Laptops",
     seller: "Xiaomi ",
     stock: 500,
-    numOfReviews: 1,
+    numOfReviews: 0,
     reviews: [],
   },
   {
@@ -273,8 +271,7 @@ export default [
     price: "40",
     description:
       "Xiaomi C300 được thiết kế để có thể quay chụp được những thước phim ở độ phân giải 2K Ultra-Clear HD cực cao, đảm bảo mọi khuôn hình đều được ghi lại theo cách chân thực và chi tiết nhất. Chất lượng hình ảnh 1296p sẽ giúp bạn thấy rõ lại bối cảnh đặt camera khi muốn kiểm tra, tua, xem lại hoặc zoom cận cảnh.",
-    ratings: 5,
-    images: [
+    ratings: 0.0,    images: [
       {
         public_id: "Product",
         url: "https://res.cloudinary.com/daf4umk5i/image/upload/v1703061090/Products/camera1_vfaax0.webp",
@@ -283,7 +280,7 @@ export default [
     category: "Cameras",
     seller: "Xiaomi ",
     stock: 500,
-    numOfReviews: 1,
+    numOfReviews: 0,
     reviews: [],
   },
   {
@@ -291,8 +288,7 @@ export default [
     price: "100",
     description:
       "Nếu bạn cho rằng camera an ninh thường khá nhòe và không thể hiển thị được hình ảnh sắc sảo thì có thể bạn đã nhầm. Camera an ninh Xiaomi Mi 360 2K Pro có thể thu được hình ảnh với độ phân giải lên đến 2.304 x 1.296 pixel. Do đó, bạn có thể thấy được từng chi tiết nhỏ trong khuôn hình và theo dõi cụ thể mọi diễn biến xảy ra ở nơi đặt camera.",
-    ratings: 5,
-    images: [
+    ratings: 0.0,    images: [
       {
         public_id: "Product",
         url: "https://res.cloudinary.com/daf4umk5i/image/upload/v1703061102/Products/camera2_ogayyx.webp",
@@ -301,7 +297,7 @@ export default [
     category: "Cameras",
     seller: "Xiaomi ",
     stock: 500,
-    numOfReviews: 1,
+    numOfReviews: 0,
     reviews: [],
   },
   {
@@ -309,8 +305,7 @@ export default [
     price: "400",
     description:
       "Apple Watch SE có thiết kế tương tự như Apple Watch Series 6, nhưng có một số thay đổi nhỏ. Viền máy được làm bằng nhôm, mặt sau được làm bằng nhựa và có một camera ở góc trên cùng bên trái. Mặt trước được trang bị màn hình Retina OLED 40 mm hoặc 44 mm với độ phân giải 324 x 394 pixel. ",
-    ratings: 4,
-    images: [
+    ratings: 0.0,    images: [
       {
         public_id: "Product",
         url: "https://res.cloudinary.com/daf4umk5i/image/upload/v1703061492/Products/applewatchse_teytqr.webp",
@@ -319,7 +314,7 @@ export default [
     category: "Smartwatchs",
     seller: "Apple ",
     stock: 500,
-    numOfReviews: 2,
+    numOfReviews: 0,
     reviews: [],
   },
   {
@@ -327,8 +322,7 @@ export default [
     price: "1000",
     description:
       " Apple Watch Ultra có thiết kế hoàn toàn mới, với vỏ được làm bằng titan chuẩn hàng không vũ trụ và mặt sau được làm bằng gốm. Máy có kích thước 49 mm và dày 10,4 mm, Apple Watch Ultra có thời lượng pin lên đến 24 giờ, giúp bạn có thể sử dụng máy cả ngày dài mà không cần lo lắng về việc hết pin. ",
-    ratings: 4,
-    images: [
+    ratings: 0.0 ,   images: [
       {
         public_id: "Product",
         url: "https://res.cloudinary.com/daf4umk5i/image/upload/v1703061779/Products/applewatchultra_iu7nqh.webp",
@@ -337,7 +331,7 @@ export default [
     category: "Smartwatchs",
     seller: "Apple ",
     stock: 500,
-    numOfReviews: 2,
+    numOfReviews: 0,
     reviews: [],
   },
   {
@@ -345,8 +339,7 @@ export default [
     price: "799",
     description:
       "  Độ bền đạt tiêu chuẩn MIL-STD 810H cùng chống bụi IP6X, Thời lượng pin lâu dài lên đến 36 tiếng.Con chip Apple S9 SiP cho tốc độ và độ sáng ấn tượng.Chạm 2 lần để sử dụng mà không cần chạm vào màn hình. Cảm biến sức khỏe tiên tiến, cung cấp thông tin chuyên sâu. Có thể gọi điện và nhận tin nhắn khi iPhone không ở gần. Trợ giúp từ SOS nếu bạn ngã hoặc va chạm ô tô.",
-    ratings: 4,
-    images: [
+    ratings: 0.0,    images: [
       {
         public_id: "Product",
         url: "https://res.cloudinary.com/daf4umk5i/image/upload/v1703061779/Products/applewatchultra_iu7nqh.webp",
@@ -355,7 +348,7 @@ export default [
     category: "Smartwatchs",
     seller: "Apple ",
     stock: 500,
-    numOfReviews: 2,
+    numOfReviews: 0,
     reviews: [],
   },
   {
@@ -363,8 +356,7 @@ export default [
     price: "250",
     description:
       "- Thiết kế độc đáo, phối màu ấn tượng. Sử dụng màng loa Graphene 50mm giúp tái tạo âm thanh vô cùng chân thực. Khả năng tương thích cao qua nhiều chế độ kết nối. Giúp loại bỏ tiếng ồn mang lại chất lượng giọng nói rõ ràng như phòng thu và loại bỏ tiếng ồn xung quanh",
-    ratings: 4,
-    images: [
+    ratings: 0.0,    images: [
       {
         public_id: "Product",
         url: "https://res.cloudinary.com/daf4umk5i/image/upload/v1703062269/Products/headphone_d9fxft.webp",
@@ -373,7 +365,7 @@ export default [
     category: "Headphones",
     seller: "Logitech ",
     stock: 500,
-    numOfReviews: 2,
+    numOfReviews: 0,
     reviews: [],
   },
   {
@@ -381,8 +373,7 @@ export default [
     price: "119",
     description:
       "Thiết kế hiện đại, mỏng nhẹ mang lại tính di động cực cao để người dùng dễ dàng mang theo bên mình.Chất liệu nhôm cao cấp, sang trọng cho khả năng chống va đập tốt hạn chế hỏng hóc khi va chạm. Cơ chế cắt kéo cùng hành trình phím chính xác và êm ái cho người sử dụng trải nghiệm tuyệt vời. Vừa sạc vừa kết nối thông qua cáp sạc Lightning và kết nối không dây Bluetooth vô cùng tiện lợi. Sử dụng pin sạc Li-ion thời lượng sử dụng lên tới 30 ngày cho mỗi lần sạc.",
-    ratings: 4,
-    images: [
+    ratings: 0.0,    images: [
       {
         public_id: "Product",
         url: "https://res.cloudinary.com/daf4umk5i/image/upload/v1703062638/Products/keyboard1_vosrvw.webp",
@@ -391,7 +382,7 @@ export default [
     category: "Accessories",
     seller: "Apple ",
     stock: 500,
-    numOfReviews: 2,
+    numOfReviews: 0,
     reviews: [],
   },
   {
@@ -399,7 +390,7 @@ export default [
     price: "1599",
     description:
       "Thiết kế tối giản, đi trước thời đại với vỏ nhôm nguyên khối chắc chắn và màn hình viền mỏng.Màn hình rộng tới 24 inch cùng độ phân giải siêu nét lên tới 5K.Hiệu suất mạnh mẽ, ổn định nhờ trang bị con chip Apple Silicon. Giá cả cạnh tranh, phù hợp với nhiều đối tượng người dùng.",
-    ratings: 4.5,
+    ratings: 0.0,
     images: [
       {
         public_id: "Product",
@@ -409,7 +400,7 @@ export default [
     category: "Electronics",
     seller: "Apple",
     stock: 100,
-    numOfReviews: 112,
+    numOfReviews: 0,
     reviews: [],
   },
   {
@@ -417,7 +408,7 @@ export default [
     price: "100",
     description:
       "Magic Mouse có thể kết nối không dây và sạc lại được, với phần đế được thiết kế tối ưu giúp di chuyển dễ dàng trên mặt bàn. Bề mặt Multi-Touch cho phép bạn thực hiện các thao tác đơn giản như vuốt giữa các trang web và cuộn qua lại giữa các tài liệu.",
-    ratings: 4.5,
+    ratings: 0.0,
     images: [
       {
         public_id: "Product",
@@ -427,7 +418,7 @@ export default [
     category: "Accessories",
     seller: "Apple",
     stock: 100,
-    numOfReviews: 112,
+    numOfReviews: 0,
     reviews: [],
   },
   {
@@ -435,7 +426,7 @@ export default [
     price: "150",
     description:
       "Có độ phân giải lên đến 4K (Ultra HD 2160p) mang đến hình ảnh chi tiết và sắc nét, độ tương phản tốt, màu sắc chân thực. Hỗ trợ chuẩn HDR10, Dolby Atmos và Dolby Vision, Chip A12 Bionic cải tiến cho khả năng xử lý đa nhiệm, cung cấp trải nghiệm mượt mà và chất lượng cao.Hỗ trợ nhiều nền tảng xem phim trực tuyến giúp bạn tận hưởng, trải nghiệm theo sở thích của mình.",
-    ratings: 4.5,
+    ratings: 0.0,
     images: [
       {
         public_id: "Product",
@@ -445,7 +436,7 @@ export default [
     category: "Electronics",
     seller: "Apple",
     stock: 100,
-    numOfReviews: 112,
+    numOfReviews: 0,
     reviews: [],
   },
   {
@@ -453,7 +444,7 @@ export default [
     price: "250",
     description:
       "Chất liệu vải mềm Fabric thoáng khí và thoải mái khi sử dụng trong nhiều giờ Đệm ngồi, lưng ghế và cơ chế ngả lưng lên đến 160 độTay ghế 2D điều chỉnh linh hoạt. Trục thủy lực Piston Class 4 cao cấp chịu tải trọng đến 120 Kg",
-    ratings: 4.5,
+    ratings: 0.0,
     images: [
       {
         public_id: "Product",
@@ -463,7 +454,7 @@ export default [
     category: "Accessories",
     seller: "CorSair",
     stock: 100,
-    numOfReviews: 112,
+    numOfReviews: 0,
     reviews: [],
   },
   {
@@ -471,7 +462,7 @@ export default [
     price: "450",
     description:
       "Thiết kế hiện đại, kiểu dáng tối giản. Trang bị bảng điều khiển AI Control hiển thị rõ ràng, dễ sử dụng. Tích hợp sẵn 22 chương trình giặt tiện lợi. Công nghệ Eco Bubble giúp đánh tan chất giặt tẩy. Giặt hơi nước diệt khuẩn Hygiene Steam. Trang bị nhiều tiện ích: Khóa trẻ em, hẹn giờ giặt, tự vệ sinh lồng giặt ... ",
-    ratings: 4.5,
+    ratings: 0.0,
     images: [
       {
         public_id: "Product",
@@ -481,7 +472,7 @@ export default [
     category: "Electronics",
     seller: "SamSung",
     stock: 100,
-    numOfReviews: 112,
+    numOfReviews: 0,
     reviews: [],
   },
   {
@@ -489,7 +480,7 @@ export default [
     price: "650",
     description:
       "Thiết kế hiện đại, sang trọng với dung tích 519L thích hợp cho gia đình 4-5 thành viênCông nghệ Linear Inverter kết hợp với Smart Inverter giúp tiết kiệm điện. Công nghệ làm lạnh đa chiều, làm mát đồng đều, bảo quản thực phẩm tối ưu. Bộ lọc khử mùi than hoạt tính giúp loại bỏ mùi hôi tanh. Bảng điều khiển bên ngoài, thao tác dễ dàng",
-    ratings: 4.5,
+    ratings: 0.0,
     images: [
       {
         public_id: "Product",
@@ -507,7 +498,7 @@ export default [
     price: "300",
     description:
       "Kích thước: 43. Độ phân giải: 3840 x 2160 / Ultra HD (4K)Hệ điều hành: TizenKết nối mạng: LAN, WiFi, Bluetooth. Cổng xuất hình: 3 x HDMI",
-    ratings: 4.5,
+    ratings: 0.0,
     images: [
       {
         public_id: "Product",
