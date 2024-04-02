d0ff966a83146e27a6d759bff51e8667c214486f
duyhungg <dongocduyhung7@gmail.com>
2024-04-02 15:37:16 +0700

    update review

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,4 +1,4 @@
-import express from 'express'
+import express from "express";
 import {
   canUserReview,
   createProductReview,
@@ -15,38 +15,45 @@ import {
   getFavouriteProduct,
   Sort,
   updateProductReview,
-} from '../controllers/productControllers.js'
-import { authorizeRoles, isAuthenticatedUser } from '../middlewares/auth.js'
-const router = express.Router()
+} from "../controllers/productControllers.js";
+import { authorizeRoles, isAuthenticatedUser } from "../middlewares/auth.js";
+const router = express.Router();
 
-router.route('/products').get(getProducts)
-router.route('/sort').get(Sort)
-router.route('/products/popular').get(getFavouriteProduct)
+router.route("/products").get(getProducts);
+router.route("/sort").get(Sort);
+router.route("/products/popular").get(getFavouriteProduct);
 router
-  .route('/admin/products')
-  .post(isAuthenticatedUser, authorizeRoles('admin'), newProduct)
-  .get(isAuthenticatedUser, authorizeRoles('admin'), getAdminProducts)
+  .route("/admin/products")
+  .post(isAuthenticatedUser, authorizeRoles("admin"), newProduct)
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getAdminProducts);
 
-router.route('/products/:id').get(getProductDetails)
-
-router
-  .route('/admin/products/:id/upload_images')
-  .put(isAuthenticatedUser, authorizeRoles('admin'), uploadProductImages)
+router.route("/products/:id").get(getProductDetails);
 
 router
-  .route('/admin/products/:id/delete_image')
-  .put(isAuthenticatedUser, authorizeRoles('admin'), deleteProductImage)
+  .route("/admin/products/:id/upload_images")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), uploadProductImages);
 
-router.route('/admin/products/:id').put(isAuthenticatedUser, authorizeRoles('admin'), updateProduct)
 router
-  .route('/admin/products/:id')
-  .delete(isAuthenticatedUser, authorizeRoles('admin'), deleteProduct)
+  .route("/admin/products/:id/delete_image")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), deleteProductImage);
 
-router.route('/reviews').get(isAuthenticatedUser, getProductReviews)
-router.route('/reviews/update').put(isAuthenticatedUser, updateProductReview)
+router
+  .route("/admin/products/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct);
+router
+  .route("/admin/products/:id")
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
 
-router.route('/admin/reviews').delete(isAuthenticatedUser, authorizeRoles('admin'), deleteReview)
+router
+  .route("/reviews")
+  .get(isAuthenticatedUser, getProductReviews)
+  .put(isAuthenticatedUser, createProductReview);
+router.route("/reviews/update").put(isAuthenticatedUser, updateProductReview);
 
-router.route('/can_review').get(isAuthenticatedUser, canUserReview)
+router
+  .route("/admin/reviews")
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteReview);
 
-export default router
+router.route("/can_review").get(isAuthenticatedUser, canUserReview);
+
+export default router;
